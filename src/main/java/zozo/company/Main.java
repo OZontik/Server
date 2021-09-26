413018a0eccb60b164ddd65c0c182dbe4e5f740c
OZontik <olgazotova116@gmail.com>
2021-09-26 15:50:02 +0300

    1

---
+++ src/main/java/zozo/company/Main.java
@@ -0,0 +1,14 @@
+package zozo.company;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        var validPath = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
+        Server server = new Server(64, validPath);
+        server.listen(9999);
+    }
+}
+
+
+
